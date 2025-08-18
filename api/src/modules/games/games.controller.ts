b75709c585f5982d85126997e0530efa79d47aa7
Guilherme Quincó <guilherme.quinco@ibbi.org.br>
2025-08-18 14:48:37 -0400

    feat: implement find one game endpoint

--- api/src/modules/games/games.controller.ts
+++ api/src/modules/games/games.controller.ts
@@ -1,13 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ListAllGamesUseCase } from './use-cases/list-all-games.use-case';
+import { FindOneGameUseCase } from './use-cases/find-one.use-case';
 
 @Controller('games')
 export class GamesController {
-  constructor(private readonly listAllGamesUseCase: ListAllGamesUseCase) {}
+  constructor(
+    private readonly listAllGamesUseCase: ListAllGamesUseCase,
+    private readonly findOneGameUseCase: FindOneGameUseCase,
+  ) {}
 
   @Get()
   async list() {
     const response = await this.listAllGamesUseCase.execute();
+
+    return response;
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: number) {
+    const response = await this.findOneGameUseCase.execute(id);
 
     return response;
   }
--- api/src/modules/games/games.module.ts
+++ api/src/modules/games/games.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
-import { RawgService } from 'src/infra/implementations/services/http /rawg/rawg.service';
+import { RawgService } from 'src/infra/implementations/services/http/rawg/rawg.service';
 import { ListAllGamesUseCase } from './use-cases/list-all-games.use-case';
 import { HttpModule } from '@nestjs/axios';
 import { GamesController } from './games.controller';
+import { FindOneGameUseCase } from './use-cases/find-one.use-case';
 
 @Module({
   imports: [HttpModule],
   controllers: [GamesController],
-  providers: [ListAllGamesUseCase, RawgService],
+  providers: [ListAllGamesUseCase, FindOneGameUseCase, RawgService],
 })
 export class GamesModule {}
